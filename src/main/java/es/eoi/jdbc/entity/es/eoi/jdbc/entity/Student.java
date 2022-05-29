f91caeb8c3277cf0330abf72cb61bdeeeb6df5b3
celiamorenovillamayor <celia_174@hotmail.com>
2022-05-29 13:17:12 +0200

    Commit 2

--- src/main/java/es/eoi/jdbc/entity/es/eoi/jdbc/entity/Student.java
+++ src/main/java/es/eoi/jdbc/entity/es/eoi/jdbc/entity/Student.java
@@ -1,5 +1,11 @@
 package es.eoi.jdbc.entity.es.eoi.jdbc.entity;
 
+import java.util.Date;
+
 public class Student {
+public int id;
+public String name;
+public String surname;
+public Date fecha = new Date();
 
 }
